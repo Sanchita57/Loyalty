7af624ed1d8d49b1eb86f455da9906a4ee715854
Sanchita57 <147394470+Sanchita57@users.noreply.github.com>
2024-09-02 22:26:46 +0530

    Create stopAll.sh

---
+++ stopAll.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+DIRNAME=`dirname $0`
+PROGNAME=`basename $0`
+
+app_list="BELK BP CBRS LEM LUMC RN SAKS SC"
+if [ $# -gt 0 ] && [[ ! " $app_list " =~ .*\ $1\ .* ]] 
+then
+  echo "stopAll.sh will run stopTomcat.sh and then stopJVMs.sh to stop event handlers and other servers for specifed products"
+  echo "if no arguments are passed products are found that have an event handler"
+ exit 1
+
+fi
+if [ "x${PROD_BASE}" = "x" ]; then
+	export PROD_BASE=$HOME
+fi
+prods=$*
+if [ $# -eq 0 ]
+then
+	prods="$(cd ~;find ${app_list} -type d -exec test -e '{}'/valuesys/shell/ehStart.sh \; -print 2>/dev/null |tr -s '\n' '_' )"
+fi
+LOG_FILE=stop_${prods// /_}.txt.`date +%Y%m%d`
+
+$DIRNAME/stopJVMs.sh $* >> $LOG_FILE
+
+if [ "x$(ps -leaf |grep "$USER " |grep -i 'org.apache.catalina.startup.Bootstrap start' | grep -v grep|wc -l)" = "x1" ]
+then
+	echo "stopping Tomcat "
+
+	$DIRNAME/stopTomcat.sh  >> $LOG_FILE
+
+	sleep 15 
+fi
+if [ "x$(ps -leaf |grep "$USER " |grep -i 'org.apache.catalina.startup.Bootstrap start' | grep -v grep|wc -l)" = "x1" ]
+	then
+		echo
+		echo "Tomcat still running"
+	else
+		echo 
+		echo "Tomcat stopped"
+fi

6ecf0de5b97c26c6b0b4ecb40a819829a6d7fd5d
Sanchita57 <147394470+Sanchita57@users.noreply.github.com>
2024-09-06 16:05:57 +0530

    Update startAll.sh

--- startAll.sh
+++ startAll.sh
@@ -2,8 +2,8 @@
 DIRNAME=`dirname $0`
 PROGNAME=`basename $0`
 
-app_list="BELK BP CBRS LEM LUMC RN SAKS SC"
-if [ $# -gt 0 ] && [[ ! " $app_list " =~ .*\ $1\ .* ]] 
+list="BP CBRS LEM LUMC RN SC"
+if [ $# -gt 0 ] && [[ ! " $list " =~ .*\ $1\ .* ]]
 then
   echo "startAll.sh will run startTomcat.sh and then startJVMs.sh to start event handlers and other servers for specifed products"
   echo "if no arguments are passed and the configuration looks viable tomcat is started without change"
@@ -12,27 +12,27 @@ then
 
 fi
 if [ "x${PROD_BASE}" = "x" ]; then
-	export PROD_BASE=$HOME
+        export PROD_BASE=$HOME
 fi
 prods=$*
 if [ $# -eq 0 ]
 then
-	prods="$(cd ${PROD_BASE};find ${app_list} -type d -exec test -e '{}'/valuesys/shell/ehStart.sh \; -print|tr -s '\n' '_' 2>/dev/null)"
+        prods="$(cd ${PROD_BASE};find BP CBRS LEM LUMC RN SC -type d -exec test -e '{}'/valuesys/shell/ehStart.sh \; -print|tr -s '\n' '_' 2>/dev/null)"
 fi
 mkdir -p logs
 LOG_FILE=start_${prods// /_}.txt.`date +%Y%m%d`
 echo "starting Tomcat $*"
 
-	$DIRNAME/startTomcat.sh $* >> $LOG_FILE
+        $DIRNAME/startTomcat.sh $* >> $LOG_FILE
 
-sleep 20 
+sleep 20
 
 if [ "x$(ps -leaf |grep "$USER "  |grep -i 'org.apache.catalina.startup.Bootstrap start' | grep -v grep|wc -l)" = "x1" ]
-	then
-		echo
-		echo "Tomcat running"
-	else
-		echo 
-		echo "Tomcat  could not be started, please do it manually"
+        then
+                echo
+                echo "Tomcat running"
+        else
+                echo
+                echo "Tomcat  could not be started, please do it manually"
 fi
 $DIRNAME/startJVMs.sh $* >> $LOG_FILE
